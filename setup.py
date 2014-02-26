6da2fac3629f411b56cceeda08c6535da4648c1e
Ian Foote <ian@incuna.com>
2014-02-26 11:37:22 +0000

    Bump to v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='incuna-mail',
-    version='0.1.2',
+    version='0.2.0',
     url='http://github.com/incuna/incuna-mail',
     license='MIT',
     author='Incuna Ltd',
