ab419dced6174e5eddc79b02832073984166b555
adam-incuna <sortoflikechess@gmail.com>
2014-06-19 16:53:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='incuna-mail',
-    version='0.2.0',
+    version='0.3.0',
     url='http://github.com/incuna/incuna-mail',
     license='MIT',
     author='Incuna Ltd',
